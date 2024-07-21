3dde5e0ca799b759b054bffd0d217d9396d29307
thinken-cyber <ducthinhn249@gmail.com>
2024-07-21 17:18:22 +0700

    C# winform.net

---
+++ Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace quanlisinhvien
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.dtpNgaysinh = new System.Windows.Forms.DateTimePicker();
+            this.txtHoten = new System.Windows.Forms.TextBox();
+            this.txtMasv = new System.Windows.Forms.TextBox();
+            this.lblNgaysinh = new System.Windows.Forms.Label();
+            this.lblHoten = new System.Windows.Forms.Label();
+            this.lblMasv = new System.Windows.Forms.Label();
+            this.btnLoc = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.radnu = new System.Windows.Forms.RadioButton();
+            this.radnam = new System.Windows.Forms.RadioButton();
+            this.lblGioitinh = new System.Windows.Forms.Label();
+            this.cboNoisinh = new System.Windows.Forms.ComboBox();
+            this.lblNoisinh = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 214;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.splitContainer2.Panel1.Controls.Add(this.dtpNgaysinh);
+            this.splitContainer2.Panel1.Controls.Add(this.txtHoten);
+            this.splitContainer2.Panel1.Controls.Add(this.txtMasv);
+            this.splitContainer2.Panel1.Controls.Add(this.lblNgaysinh);
+            this.splitContainer2.Panel1.Controls.Add(this.lblHoten);
+            this.splitContainer2.Panel1.Controls.Add(this.lblMasv);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.splitContainer2.Panel2.Controls.Add(this.btnLoc);
+            this.splitContainer2.Panel2.Controls.Add(this.btnXoa);
+            this.splitContainer2.Panel2.Controls.Add(this.btnSua);
+            this.splitContainer2.Panel2.Controls.Add(this.btnThem);
+            this.splitContainer2.Panel2.Controls.Add(this.radnu);
+            this.splitContainer2.Panel2.Controls.Add(this.radnam);
+            this.splitContainer2.Panel2.Controls.Add(this.lblGioitinh);
+            this.splitContainer2.Panel2.Controls.Add(this.cboNoisinh);
+            this.splitContainer2.Panel2.Controls.Add(this.lblNoisinh);
+            this.splitContainer2.Size = new System.Drawing.Size(800, 214);
+            this.splitContainer2.SplitterDistance = 392;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // dtpNgaysinh
+            // 
+            this.dtpNgaysinh.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpNgaysinh.Location = new System.Drawing.Point(95, 162);
+            this.dtpNgaysinh.Name = "dtpNgaysinh";
+            this.dtpNgaysinh.Size = new System.Drawing.Size(264, 26);
+            this.dtpNgaysinh.TabIndex = 5;
+            this.dtpNgaysinh.Value = new System.DateTime(2000, 1, 1, 21, 52, 0, 0);
+            // 
+            // txtHoten
+            // 
+            this.txtHoten.Location = new System.Drawing.Point(95, 104);
+            this.txtHoten.Name = "txtHoten";
+            this.txtHoten.Size = new System.Drawing.Size(267, 26);
+            this.txtHoten.TabIndex = 4;
+            // 
+            // txtMasv
+            // 
+            this.txtMasv.Location = new System.Drawing.Point(95, 40);
+            this.txtMasv.Name = "txtMasv";
+            this.txtMasv.Size = new System.Drawing.Size(267, 26);
+            this.txtMasv.TabIndex = 3;
+            // 
+            // lblNgaysinh
+            // 
+            this.lblNgaysinh.AutoSize = true;
+            this.lblNgaysinh.Location = new System.Drawing.Point(3, 162);
+            this.lblNgaysinh.Name = "lblNgaysinh";
+            this.lblNgaysinh.Size = new System.Drawing.Size(78, 20);
+            this.lblNgaysinh.TabIndex = 2;
+            this.lblNgaysinh.Text = "Ngày sinh";
+            // 
+            // lblHoten
+            // 
+            this.lblHoten.AutoSize = true;
+            this.lblHoten.Location = new System.Drawing.Point(12, 104);
+            this.lblHoten.Name = "lblHoten";
+            this.lblHoten.Size = new System.Drawing.Size(57, 20);
+            this.lblHoten.TabIndex = 1;
+            this.lblHoten.Text = "Họ tên";
+            // 
+            // lblMasv
+            // 
+            this.lblMasv.AutoSize = true;
+            this.lblMasv.Location = new System.Drawing.Point(12, 46);
+            this.lblMasv.Name = "lblMasv";
+            this.lblMasv.Size = new System.Drawing.Size(53, 20);
+            this.lblMasv.TabIndex = 0;
+            this.lblMasv.Text = "Mã Sv";
+            // 
+            // btnLoc
+            // 
+            this.btnLoc.Location = new System.Drawing.Point(304, 168);
+            this.btnLoc.Name = "btnLoc";
+            this.btnLoc.Size = new System.Drawing.Size(77, 32);
+            this.btnLoc.TabIndex = 8;
+            this.btnLoc.Text = "Lọc";
+            this.btnLoc.UseVisualStyleBackColor = true;
+            this.btnLoc.Click += new System.EventHandler(this.btnLoc_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(214, 168);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(74, 32);
+            this.btnXoa.TabIndex = 7;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(118, 167);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(80, 33);
+            this.btnSua.TabIndex = 6;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(16, 168);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(85, 32);
+            this.btnThem.TabIndex = 5;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // radnu
+            // 
+            this.radnu.AutoSize = true;
+            this.radnu.Location = new System.Drawing.Point(275, 108);
+            this.radnu.Name = "radnu";
+            this.radnu.Size = new System.Drawing.Size(54, 24);
+            this.radnu.TabIndex = 4;
+            this.radnu.TabStop = true;
+            this.radnu.Text = "Nữ";
+            this.radnu.UseVisualStyleBackColor = true;
+            this.radnu.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radnam
+            // 
+            this.radnam.AutoSize = true;
+            this.radnam.Location = new System.Drawing.Point(144, 106);
+            this.radnam.Name = "radnam";
+            this.radnam.Size = new System.Drawing.Size(67, 24);
+            this.radnam.TabIndex = 3;
+            this.radnam.TabStop = true;
+            this.radnam.Text = "Nam";
+            this.radnam.UseVisualStyleBackColor = true;
+            // 
+            // lblGioitinh
+            // 
+            this.lblGioitinh.AutoSize = true;
+            this.lblGioitinh.Location = new System.Drawing.Point(12, 110);
+            this.lblGioitinh.Name = "lblGioitinh";
+            this.lblGioitinh.Size = new System.Drawing.Size(67, 20);
+            this.lblGioitinh.TabIndex = 2;
+            this.lblGioitinh.Text = "Giới tính";
+            // 
+            // cboNoisinh
+            // 
+            this.cboNoisinh.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cboNoisinh.FormattingEnabled = true;
+            this.cboNoisinh.Items.AddRange(new object[] {
+            "hanoi",
+            "haiphong ",
+            "da nang",
+            "lao cai",
+            "yen bai"});
+            this.cboNoisinh.Location = new System.Drawing.Point(100, 38);
+            this.cboNoisinh.Name = "cboNoisinh";
+            this.cboNoisinh.Size = new System.Drawing.Size(272, 28);
+            this.cboNoisinh.TabIndex = 1;
+            // 
+            // lblNoisinh
+            // 
+            this.lblNoisinh.AutoSize = true;
+            this.lblNoisinh.Location = new System.Drawing.Point(12, 38);
+            this.lblNoisinh.Name = "lblNoisinh";
+            this.lblNoisinh.Size = new System.Drawing.Size(65, 20);
+            this.lblNoisinh.TabIndex = 0;
+            this.lblNoisinh.Text = "Nơi sinh";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 232);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Mã sv";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Họ tên";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Ngày  sinh";
+            this.Column3.MinimumWidth = 8;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Giới tính";
+            this.Column4.MinimumWidth = 8;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Nơi sinh";
+            this.Column5.MinimumWidth = 8;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Quản lý ính viên-Nguyễn Đức Thịnh";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.DateTimePicker dtpNgaysinh;
+        private System.Windows.Forms.TextBox txtHoten;
+        private System.Windows.Forms.TextBox txtMasv;
+        private System.Windows.Forms.Label lblNgaysinh;
+        private System.Windows.Forms.Label lblHoten;
+        private System.Windows.Forms.Label lblMasv;
+        private System.Windows.Forms.Button btnLoc;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.RadioButton radnu;
+        private System.Windows.Forms.RadioButton radnam;
+        private System.Windows.Forms.Label lblGioitinh;
+        private System.Windows.Forms.ComboBox cboNoisinh;
+        private System.Windows.Forms.Label lblNoisinh;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quanlisinhvien
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            string gioitinh="";
+            if(radnam.Checked)
+            {
+               gioitinh= radnam.Text+"";
+            }else if(radnu.Checked)
+            {
+                gioitinh=radnu.Text+"";
+            }
+            dataGridView1.Rows.Add(txtMasv.Text, txtHoten.Text, dtpNgaysinh.Text,gioitinh, cboNoisinh.Text);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtMasv.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtHoten.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            dtpNgaysinh.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            if (dataGridView1.CurrentRow.Cells[3].Value == radnam.Text)
+            {
+                radnam.Checked = true;
+            }else if (dataGridView1.CurrentRow.Cells[3].Value.ToString() == radnu.Text)
+            {
+                radnu.Checked = true;
+            }
+            cboNoisinh.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            dataGridView1.CurrentRow.Cells[0].Value= txtMasv.Text;
+            dataGridView1.CurrentRow.Cells[1].Value =  txtHoten.Text;
+            dataGridView1.CurrentRow.Cells[2].Value = dtpNgaysinh.Text;
+            if (dataGridView1.CurrentRow.Cells[3].Value==radnam.Text)
+            {
+                dataGridView1.CurrentRow.Cells[3].Value = radnu.Text;
+                radnu.Checked = true;
+            }else if (dataGridView1.CurrentRow.Cells[3].Value == radnu.Text)
+            {
+                dataGridView1.CurrentRow.Cells[3].Value=radnam.Text;
+                radnam.Checked= true;
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+           int RowIndex=dataGridView1.CurrentCell.RowIndex;
+            dataGridView1.Rows.RemoveAt(RowIndex);
+        }
+
+        private void btnLoc_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show(); 
+            //mo ra 1 giao dien moi
+        }
+    }
+}
